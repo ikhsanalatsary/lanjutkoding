27ef9eb0842a257c2f96957cb5b18fc730d042a7
ikhsanalatsary <skull.saders18@gmail.com>
2021-03-24 00:47:44 +0700

    add webhook api

---
+++ pages/api/webhook.ts
@@ -0,0 +1,124 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { inspect } from 'util';
+import { request } from '@octokit/request';
+import type { NextApiRequest as Req, NextApiResponse as Res } from 'next';
+import { URL } from 'url';
+
+type UnwrappedPromiseType<T extends (...args: any) => any> = T extends (
+  ...args: any
+) => Promise<infer U>
+  ? U
+  : T extends (...args: any) => infer U
+  ? U
+  : any;
+
+async function getImageAsBuffer(imageUrl: string): Promise<string> {
+  // @ts-ignore
+  let result = await fetch(imageUrl);
+  let data = await result.buffer();
+  return data.toString('base64');
+}
+
+let requestWithAuth = request.defaults({
+  headers: {
+    authorization: 'token ' + process.env.GITHUB_TOKEN,
+  },
+});
+
+async function getLastCommitSha() {
+  let result = await requestWithAuth(
+    'GET /repos/{owner}/{repo}/git/matching-refs/{ref}',
+    {
+      owner: process.env.GITHUB_USERNAME!,
+      repo: process.env.GITHUB_REPO!,
+      ref: process.env.GITHUB_REF!,
+    }
+  );
+
+  return result.data.length ? result.data[0].object.sha : '';
+}
+
+async function createCommit(
+  fileName: string,
+  data: UnwrappedPromiseType<typeof createBlob>,
+  lastCommitSha: string
+) {
+  let tree = await requestWithAuth('POST /repos/{owner}/{repo}/git/trees', {
+    owner: process.env.GITHUB_USERNAME!,
+    repo: process.env.GITHUB_REPO!,
+    tree: [
+      {
+        path: 'public/' + fileName,
+        mode: '100644',
+        type: 'blob',
+        sha: data.sha,
+      },
+    ],
+    base_tree: lastCommitSha,
+  });
+  let result = await requestWithAuth('POST /repos/{owner}/{repo}/git/commits', {
+    owner: process.env.GITHUB_USERNAME!,
+    repo: process.env.GITHUB_REPO!,
+    message: 'add new image ' + fileName,
+    tree: tree.data.sha,
+    parents: [lastCommitSha],
+  });
+
+  return result.data;
+}
+
+async function createBlob(
+  base64Image: string
+): Promise<{ sha: string; url: string }> {
+  const result = await requestWithAuth('POST /repos/{owner}/{repo}/git/blobs', {
+    owner: process.env.GITHUB_USERNAME!,
+    repo: process.env.GITHUB_REPO!,
+    content: base64Image,
+    encoding: 'base64',
+  });
+
+  return result.data;
+}
+
+async function pushCommit(data: UnwrappedPromiseType<typeof createCommit>) {
+  let result = await requestWithAuth(
+    'PATCH /repos/{owner}/{repo}/git/refs/{ref}',
+    {
+      owner: 'ikhsanalatsary',
+      repo: 'dddd',
+      ref: process.env.GITHUB_REF!,
+      sha: data.sha,
+      // force: false,
+    }
+  );
+
+  return result.data;
+}
+
+export default async (req: Req, res: Res) => {
+  let message = '';
+  try {
+    if (req.method === 'POST') {
+      // Process a POST request
+      if (req.body.post?.post_status === 'publish') {
+        let postThumbnail = req.body.post_thumbnail; // string or false
+        if (postThumbnail) {
+          let fileName = new URL(postThumbnail).pathname.slice(28);
+          let base64Image = await getImageAsBuffer(postThumbnail);
+          let blobData = await createBlob(base64Image);
+          let lastCommit = await getLastCommitSha();
+          let commit = await createCommit(fileName, blobData, lastCommit);
+          let result = await pushCommit(commit);
+          console.log(inspect(result, true, 8));
+        }
+      }
+      message = 'success';
+    }
+  } catch (error) {
+    console.error(error);
+    message = error.message;
+  }
+  res.status(200).json({ message });
+};
