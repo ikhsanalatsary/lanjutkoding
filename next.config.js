4e9dc135ba3f9727c2118f0de843d50a72f65c9e
ikhsanalatsary <skull.saders18@gmail.com>
2021-03-25 00:03:05 +0700

    ignore type error while building

--- next.config.js
+++ next.config.js
@@ -8,4 +8,11 @@ module.exports = {
       '1.gravatar.com',
     ],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
