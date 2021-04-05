39dabb0542497854c9d8276867f4df6f63695ef9
ikhsanalatsary <skull.saders18@gmail.com>
2021-04-05 12:23:03 +0700

    move

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,15 +37,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={this.props.locale}>
-        <Head>
-          {process.env.NODE_ENV === 'production' && (
-            <script
-              defer
-              src="https://static.cloudflareinsights.com/beacon.min.js"
-              data-cf-beacon='{"token": "15a270292cfc4f5684025d07ab4fc1ab"}'
-            ></script>
-          )}
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
