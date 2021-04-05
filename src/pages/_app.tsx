38f816438dbbe9f1783df4e0a64e3a96505d8521
Abdul Fattah Ikhsan <skull.saders18@gmail.com>
2021-04-05 12:31:42 +0700

    Analytics (#13)
    * cloudflare analytic
    
    * move

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -50,6 +50,13 @@ function MyApp({ Component, pageProps }: Props) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#ffffff" />
+        {process.env.NODE_ENV === 'production' && (
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon={`{"token": "15a270292cfc4f5684025d07ab4fc1ab"}`}
+          ></script>
+        )}
       </Head>
       <div className="container mt-4 mx-auto md:px-12">
         <Component {...pageProps} />
