b7bbfe8f0b229fa3247159ace224a1828bc93baf
ikhsanalatsary <skull.saders18@gmail.com>
2021-04-11 20:33:58 +0700

    exclude uncategorized

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,5 +2,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  exclude: ['/hello-world'],
+  exclude: ['/hello-world', '/topik/uncategorized'],
 };
