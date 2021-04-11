c6201398844e9c9ac623852a7ccb865c762025a3
ikhsanalatsary <skull.saders18@gmail.com>
2021-04-11 20:28:22 +0700

    config sitemap robot

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 let defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  mode: 'jit',
+  mode: process.env.NODE_ENV === 'development' ? 'jit' : undefined,
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
