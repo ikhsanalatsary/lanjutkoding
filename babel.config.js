a49dba6c4593ed66807ce86af77dc1361ab77c07
ikhsanalatsary <skull.saders18@gmail.com>
2021-04-14 16:37:18 +0700

    support typscript & json syntax

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "presets": ["next/babel"],
   "plugins": [
     ["prismjs", {
-      "languages": ["javascript", "css", "html", "php"],
+      "languages": ["javascript", "css", "html", "php", "typescript", "json"],
       "plugins": ["line-numbers", "show-language"],
       "theme": "okaidia",
       "css": true
