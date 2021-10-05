9e9c77051c3e935305330fa8ff9193256ec8031a
ikhsanalatsary <skull.saders18@gmail.com>
2021-10-06 01:54:44 +0700

    add docker in languages list & update prismjs

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "presets": ["next/babel"],
   "plugins": [
     ["prismjs", {
-      "languages": ["javascript", "css", "html", "php", "typescript", "json"],
+      "languages": ["javascript", "css", "html", "php", "typescript", "json", "docker"],
       "plugins": ["line-numbers", "show-language"],
       "theme": "okaidia",
       "css": true
