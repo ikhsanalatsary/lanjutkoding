72f717abdf8201d6dbfe4efb7c8b2a961141fd61
Abdul Fattah Ikhsan <skull.saders18@gmail.com>
2023-01-10 09:42:29 +0700

    feat: google ads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,37 +25,27 @@ function MyApp({ Component, pageProps }: Props) {
       <Head>
         <title>Lanjutkoding.com</title>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#ffffff" />
         {process.env.NODE_ENV === 'production' && (
-          <script
-            defer
-            src="https://static.cloudflareinsights.com/beacon.min.js"
-            data-cf-beacon={`{"token": "15a270292cfc4f5684025d07ab4fc1ab"}`}
-          ></script>
+          <>
+            <script
+              defer
+              src="https://static.cloudflareinsights.com/beacon.min.js"
+              data-cf-beacon={`{"token": "15a270292cfc4f5684025d07ab4fc1ab"}`}
+            ></script>
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2154910111615603"
+              crossorigin="anonymous"
+            ></script>
+          </>
         )}
       </Head>
       <div className="container mt-4 mx-auto md:px-12">
